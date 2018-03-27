e524885e3d2e736671c3230f5f7c49bba2acb0e4
Darwin Gualotuña <daosgc@gmail.com>
2018-03-27 14:57:02 -0500

    add campaigns column

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,6 +16,7 @@ import Footer from 'Components/Footer/Footer';
 // Pages
 import CampaignsPage from '../Campaigns/CampaignsPage';
 import NotificationsPage from '../Notifications/NotificationsPage';
+import { Card, Cupon, Campaign } from 'coupon-components';
 import CouponsPage from '../Coupons/CouponsPage';
 
 const PageHome = (props) => <div><h1>Home</h1></div>
@@ -50,6 +51,32 @@ class Home extends Component {
       name: 'Carbon Burguer',
       image: 'https://i.pinimg.com/564x/bc/c8/10/bcc8102f42e58720355ca02d833c204b.jpg',
     };
+    const pizzaHut = {
+      maker: {
+        image: 'https://aalfredosalinas.files.wordpress.com/2010/09/pizza-hut-logo1.jpg',
+        cupons: 125,
+        hunted: 50
+      },
+      cupon: {
+        image: 'https://i2.wp.com/food.thecookbk.com/wp-content/uploads/2017/10/pizza-hut.jpg?fit=800%2C600',
+        date: '11 Feb - 20 May 2017',
+        promo: 'Martes 2 x 1 en pizzas',
+        address: '12-34 Downtown S.Q'
+      }
+    }
+    const starbucks = {
+      maker: {
+        image: 'https://pbs.twimg.com/profile_images/968173455580397568/Qe0pSZTk_400x400.jpg',
+        cupons: 345,
+        hunted: 2
+      },
+      cupon: {
+        image: 'https://assets3.thrillist.com/v1/image/2439237/size/gn-gift_guide_variable_c.jpg',
+        date: '11 Feb - 20 May 2017',
+        promo: 'Enjoy 50% off any Starbucks drink',
+        address: '901 S Miami Ave #106, Miami, FL 33130, EE. UU.'
+      }
+    }
 
     if(error) return <h4>{error.message}</h4>
     else if(loading) return <h4>Loading...</h4>
@@ -57,14 +84,26 @@ class Home extends Component {
     return (
       <div className={styles.container}>
         <Header tabs={tabOptions} userData={userData}/>
-        <main className={styles.renderContainer}>
-          <Switch>
-            <Route exact path='/' component={PageHome} />
-            <Route path='/campaigns' component={CampaignsPage} />
-            <Route path='/new_coupon' component={CouponsPage} />
-            <Route path='/notifications' component={NotificationsPage} />
-          </Switch>
-        </main>
+        <div className={styles.mainView}>
+          <div className={styles.lefPanel}>
+            <Card title="Campañas" subtitle="Activas">
+              <Cupon data={pizzaHut} className={styles.campaign}/>
+              <Cupon data={starbucks} className={styles.campaign}/>
+            </Card>
+            <Card title="Campañas" subtitle="Inactivas">
+              <Campaign data={pizzaHut} className={styles.campaign}/>
+              <Campaign data={starbucks} className={styles.campaign}/>
+            </Card>
+          </div>
+          <main className={styles.renderContainer}>
+            <Switch>
+              <Route exact path='/' component={PageHome} />
+              <Route path='/campaigns' component={CampaignsPage} />
+              <Route path='/new_coupon' component={CouponsPage} />
+              <Route path='/notifications' component={NotificationsPage} />
+            </Switch>
+          </main>
+        </div>
         <Footer/>
       </div>
     );
