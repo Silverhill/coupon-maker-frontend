1d405a57f79b4ee0348ed72ac0e723cfb4ac9e73
Darwin Gualotuña <daosgc@gmail.com>
2018-03-31 17:50:44 -0500

    add routes to show and create campaing

---
+++ src/pages/Campaing/CampaingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CampaingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Show Details Campaign Page</h2>
+      </div>
+    )
+  }
+}
+
+export default CampaingPage;
---
+++ src/pages/Campaing/NewCampaing/NewCampaingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewCampaingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Create Campaign Page</h2>
+      </div>
+    )
+  }
+}
+
+export default NewCampaingPage;
