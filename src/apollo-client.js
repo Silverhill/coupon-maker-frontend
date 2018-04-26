0a21abc7db86bd8414eb27f722f92a0c049c1075
Darwin Gualotuña <daosgc@gmail.com>
2018-04-26 10:23:03 -0500

    Update queries

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,5 +1,4 @@
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
 import { ApolloLink, concat } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client'
