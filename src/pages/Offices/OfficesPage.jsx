332643e6cd194aa965dc3d3bc9cce6b90c0c7a89
Lenin Capa <leviskp87@gmail.com>
2018-05-04 16:16:20 -0500

    refactor office page to Query

--- src/pages/Offices/OfficesPage.jsx
+++ src/pages/Offices/OfficesPage.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Typography, Icon, Panel, Card, Cover, InputFile } from 'coupon-components';
+import { Typography, Icon, Panel, Card, Cover, InputFile, Menu } from 'coupon-components';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { NavLink } from 'react-router-dom';
 import RowOffice from './partials/RowOffice';
 
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import { makerOffices, getMyCompany } from 'Services/graphql/queries.graphql';
 import { withApollo } from 'react-apollo';
 import { changeLogoCompany } from 'Services/graphql/queries.graphql';
@@ -57,34 +57,42 @@ class OfficesPage extends Component {
 
 
   render() {
-    const { intl, data: { myOffices, myCompany } } = this.props;
+    const { intl } = this.props;
     const { currentOffice, isLoadingImage } = this.state;
-    const total = myOffices ? myOffices.length : 0;
-    const company = myCompany ? myCompany : {};
-    let placeholderCompany = 'http://www.lavenderceiling.com/wp-content/uploads/2016/08/logo_placehold.jpg';
-    let companyLogo = company && company.logo ? company.logo : placeholderCompany;
 
-    const tableOffices = (
-      <div className={styles.table}>
-        {myOffices && myOffices.map((office) => {
-          const key = { key: office.id };
-          let isOpen = office.id === currentOffice;
+    const companyquery = (
+      <Query query={getMyCompany}>
+        {({ loading, error, data}) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+          const { myCompany } = data;
+          let placeholderCompany = 'http://www.lavenderceiling.com/wp-content/uploads/2016/08/logo_placehold.jpg';
+          let companyLogo = myCompany.logo || placeholderCompany;
+
           return (
-            <RowOffice {...key}
-              data={office}
-              className={styles.row}
-              isOpen={isOpen}
-              onClick={()=>{this.selectRowOffice(office)}}
-            />
-          )
-        })}
-        <div className={styles.linkBtn}>
-          <NavLink to='/new_office' className={styles.link}>
-            <FormattedMessage id='myOffices.new' />
-          </NavLink>
-        </div>
-      </div>
+            <div className={styles.companyView}>
+              <InputFile updateFile={this.changeImage} isLoading={isLoadingImage} className={styles.cover}>
+                <Cover
+                  logo={companyLogo}
+                  backgroundColor="#f7f7f7"
+                  image="https://previews.123rf.com/images/flameandstar/flameandstar1511/flameandstar151100006/48071268-life-in-the-city-picture-in-flat-design-blue-background-taxi-cars-bicycle.jpg"
+                  leftLabel={intl.formatMessage({id: 'myCompany.slogan'})}
+                  leftText={myCompany.slogan || "Aqui va tu slogan (Ejm, Enganchate conmigo)" }
+                  rightLabel={intl.formatMessage({id: 'myCompany.company'})}
+                  rightText={myCompany.businessName}
+                />
+              </InputFile>
+              <Menu className={styles.menu}
+                iconOptions={{name: "FaCog", size: 20, color:"white"}}
+                options={[{label: "Editar", iconName: "FaEdit"}]}
+                onChange={this.changeMenu}
+              />
+            </div>
+          );
+        }}
+      </Query>
     )
+
     const emptyState = (
       <div className={styles.emptyState}>
         <Icon
@@ -117,30 +125,56 @@ class OfficesPage extends Component {
       </div>
     )
 
+    const tableOffices = (
+      <Query query={makerOffices}>
+        {({ loading, error, data}) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+          const { myOffices } = data;
+          const total = myOffices ? myOffices.length : 0;
+
+          return (
+            <div>
+              { total === 0 && emptyState}
+              { total > 0 &&
+                <div className={styles.table}>
+                  {myOffices && myOffices.map((office) => {
+                    const key = { key: office.id };
+                    let isOpen = office.id === currentOffice;
+                    return (
+                      <RowOffice {...key}
+                        data={office}
+                        className={styles.row}
+                        isOpen={isOpen}
+                        onClick={()=>{this.selectRowOffice(office)}}
+                      />
+                    )
+                  })}
+                  <div className={styles.linkBtn}>
+                    <NavLink to='/new_office' className={styles.link}>
+                      <FormattedMessage id='myOffices.new' />
+                    </NavLink>
+                  </div>
+                </div>
+              }
+            </div>
+          );
+        }}
+      </Query>
+    )
+
     return (
       <Card title={intl.formatMessage({id: 'myCompany.title'})}
             classNameCard={styles.offices}
             style={{position: 'relative'}}>
-        <InputFile updateFile={this.changeImage} isLoading={isLoadingImage}>
-          <Cover
-            logo={companyLogo}
-            backgroundColor="#f7f7f7"
-            image="https://previews.123rf.com/images/flameandstar/flameandstar1511/flameandstar151100006/48071268-life-in-the-city-picture-in-flat-design-blue-background-taxi-cars-bicycle.jpg"
-            leftLabel={intl.formatMessage({id: 'myCompany.slogan'})}
-            leftText="Aqui va tu slogan(Ejm, Enganchate conmigo)"
-            rightLabel={intl.formatMessage({id: 'myCompany.company'})}
-            rightText={company.businessName}/>
-        </InputFile>
+        {companyquery}
         <Panel title={intl.formatMessage({id: 'myOffices.panelTitle'})}
           className={styles.panel}>
-          { total === 0 && emptyState}
-          { total > 0 && tableOffices}
+          {tableOffices}
         </Panel>
       </Card>
     )
   }
 }
 
-export default graphql(makerOffices, { options: () => ({
-  variables: { withCompany: true }
-}) })(withApollo(injectIntl(OfficesPage)));
+export default withApollo(injectIntl(OfficesPage));
