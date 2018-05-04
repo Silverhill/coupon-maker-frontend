e95eaa6f611b6b089cd003c08b03010a72622769
Darwin Gualotuña <daosgc@gmail.com>
2018-05-04 17:11:54 -0500

    remove comments

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,8 +1,6 @@
 export const maxnum=(num, max, reduce, decimals)=>{
-  // Default values
   if (max === undefined || max === 0) {
-    //max = 999999999999999; // 999T something is breaking with this...limit?
-    max = 99999999999999; // 99T...things break somewhere between 99T and 999T
+    max = 99999999999999;
   }
   if (reduce === undefined) {
     reduce = true;
@@ -11,21 +9,15 @@ export const maxnum=(num, max, reduce, decimals)=>{
     decimals = 2;
   }
 
-  // Coerce to number, and track original value
   var actualNum = num = Number(num);
 
-  // Other parts we might add to formatted value.
-  // Return value is num + unit + plus. These get
-  // filled as necessary below.
   var unit = '';
   var plus = '';
 
-  // Quick return for edge cases
   if (!num) {
     return '0';
   }
 
-  // Cap the number?
   if (num > max) {
     num = max;
     plus = '+';
@@ -33,7 +25,6 @@ export const maxnum=(num, max, reduce, decimals)=>{
   }
 
 
-  // Reduce the number?
   if (reduce) {
 
     var units = ['', 'K', 'M', 'B', 'T'];
@@ -41,7 +32,7 @@ export const maxnum=(num, max, reduce, decimals)=>{
     unit = units[i];
 
     num /= Math.pow(1000, i);
-    num = num.toFixedDown(decimals); // round decimals down
+    num = num.toFixedDown(decimals);
 
     if ((actualNum > 1000) && (actualNum % 1000 !== 0)) {
       plus = '+';
