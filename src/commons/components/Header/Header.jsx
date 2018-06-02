e9bf9566c94d7a3dae06ab5960fa8425410ce905
Darwin Gualotuña <daosgc@gmail.com>
2018-06-02 17:39:31 -0500

    add subscriptions into header

--- src/commons/components/Header/Header.jsx
+++ src/commons/components/Header/Header.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { getMe } from 'Services/graphql/queries.graphql';
+import { huntedCoupons } from 'Services/graphql/suscriptions.graphql';
+import { Subscription } from 'react-apollo';
+import ToastTemplate from 'Components/ToastTemplate/ToastTemplate';
+import { toast } from 'react-toastify';
+import { injectIntl } from 'react-intl';
 import { Avatar, Typography, Icon, Dropdown, DropdownTrigger, DropdownContent } from 'coupon-components';
 import PropTypes from 'prop-types'
 import classNames from 'classnames/bind';
@@ -19,77 +24,96 @@ class Header extends React.Component {
     this.setState({ menuIsOpen: isOpen });
   }
 
+  showSuccessNotification = (code) => {
+    const { intl } = this.props;
+    let title = intl.formatMessage({id: 'coupons.toasts.success.hunted.title'})+':'+code;
+    toast(
+      <ToastTemplate
+        title={title}
+        subtitle={intl.formatMessage({id: 'coupons.toasts.success.hunted.subtitle'})}
+        status='success'
+      />
+    )
+  }
+
   render() {
     const { tabs, optionsUser } = this.props;
     const { menuIsOpen } = this.state;
 
     return (
-      <div className={cx(styles.container)}>
-        <div className={cx(styles.logo)}>
-          <NavLink to='/'>
-            <Logo color={primaryColor}/>
-            <div className={cx(styles.dividerLogo)}></div>
-            <Typography.Subtitle light style={{color: primaryColor}}>MAKER</Typography.Subtitle>
-          </NavLink>
-        </div>
-        <div className={cx(styles.itemsContainer)}>
-        <div className={cx(styles.optionTabs)}>
-          {tabs && tabs.map((tab) => {
-            const key = { key: tab.id };
-            return (
-              <NavLink {...key} to={tab.route} className={styles.tabs} activeClassName={styles.active}>
-                <Icon name={tab.icon} size={30} />
-                <Typography.Text small bold>{tab.label}</Typography.Text>
+      <Subscription subscription={huntedCoupons}>{({ data, loading}) => {
+        if(data && data.huntedCoupon){
+          this.showSuccessNotification(data.huntedCoupon.code);
+        }
+        return(
+          <div className={cx(styles.container)}>
+            <div className={cx(styles.logo)}>
+              <NavLink to='/'>
+                <Logo color={primaryColor}/>
+                <div className={cx(styles.dividerLogo)}></div>
+                <Typography.Subtitle light style={{color: primaryColor}}>MAKER</Typography.Subtitle>
               </NavLink>
-            )
-          })}
-        </div>
-        <Query query={getMe}>{({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error! ${error.message}`;
-          const {me} = data;
-          const image = me.image;
+            </div>
+            <div className={cx(styles.itemsContainer)}>
+            <div className={cx(styles.optionTabs)}>
+              {tabs && tabs.map((tab) => {
+                const key = { key: tab.id };
+                return (
+                  <NavLink {...key} to={tab.route} className={styles.tabs} activeClassName={styles.active}>
+                    <Icon name={tab.icon} size={30} />
+                    <Typography.Text small bold>{tab.label}</Typography.Text>
+                  </NavLink>
+                )
+              })}
+            </div>
+            <Query query={getMe}>{({ loading, error, data }) => {
+              if (loading) return "Loading...";
+              if (error) return `Error! ${error.message}`;
+              const {me} = data;
+              const image = me.image;
 
-          return (
-            <Dropdown isOpen={this.onMenuIsOpen}>
-              <DropdownTrigger>
-                <div>
-                  <div className={cx(styles.userMenu)}>
-                    <div className={styles.profileContainer}>
-                      <div className={cx(styles.avatarContainer)}>
-                        <Avatar image={image} borderColor='accentColorSecondary' />
+              return (
+                <Dropdown isOpen={this.onMenuIsOpen}>
+                  <DropdownTrigger>
+                    <div>
+                      <div className={cx(styles.userMenu)}>
+                        <div className={styles.profileContainer}>
+                          <div className={cx(styles.avatarContainer)}>
+                            <Avatar image={image} borderColor='accentColorSecondary' />
+                          </div>
+                          <Typography.Text small bold style={{margin: "0 10px"}}>{me.name}</Typography.Text>
+                        </div>
+                        <Icon name={menuIsOpen ? 'FaCaretUp' : 'FaCaretDown'} size={15}/>
                       </div>
-                      <Typography.Text small bold style={{margin: "0 10px"}}>{me.name}</Typography.Text>
                     </div>
-                    <Icon name={menuIsOpen ? 'FaCaretUp' : 'FaCaretDown'} size={15}/>
-                  </div>
-                </div>
-              </DropdownTrigger>
-              <DropdownContent className={styles.menuContainer}>
-                <div className={styles.infoContainer}>
-                  <div className={styles.avatarInfo}>
-                    <Avatar image={image}/>
-                  </div>
-                  <div className={styles.profileInfo}>
-                    <Typography.Text small>{me.name}</Typography.Text>
-                    <Typography.Label>{me.email}</Typography.Label>
-                  </div>
-                </div>
-                {optionsUser.options && optionsUser.options.map((option, i) => {
-                  return (
-                    <div key={`option-${i}`} onClick={option.onClick} className={styles.menuOption}>
-                      <Typography.Text small>
-                        {option.value}
-                      </Typography.Text>
+                  </DropdownTrigger>
+                  <DropdownContent className={styles.menuContainer}>
+                    <div className={styles.infoContainer}>
+                      <div className={styles.avatarInfo}>
+                        <Avatar image={image}/>
+                      </div>
+                      <div className={styles.profileInfo}>
+                        <Typography.Text small>{me.name}</Typography.Text>
+                        <Typography.Label>{me.email}</Typography.Label>
+                      </div>
                     </div>
-                  )
-                })}
-              </DropdownContent>
-            </Dropdown>
-          );
-        }}</Query>
-      </div>
-    </div>
+                    {optionsUser.options && optionsUser.options.map((option, i) => {
+                      return (
+                        <div key={`option-${i}`} onClick={option.onClick} className={styles.menuOption}>
+                          <Typography.Text small>
+                            {option.value}
+                          </Typography.Text>
+                        </div>
+                      )
+                    })}
+                  </DropdownContent>
+                </Dropdown>
+              );
+            }}</Query>
+          </div>
+        </div>
+      );
+    }}</Subscription>
   );
   }
 }
@@ -99,4 +123,4 @@ Header.propTypes = {
   tabs: PropTypes.array,
 }
 
-export default Header
+export default (injectIntl(Header));
